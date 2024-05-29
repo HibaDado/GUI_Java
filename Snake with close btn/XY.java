eeefd9b3c3e844fff0600df3e404508e2faa3b09
HibaDado <163197604+HibaDado@users.noreply.github.com>
2024-05-29 16:59:37 +0300

    Add files via upload

---
+++ Snake with close btn/XY.java
@@ -0,0 +1,27 @@
+package Snake_35;
+
+public class XY {
+    private int x;
+    private int y;
+
+    public XY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
